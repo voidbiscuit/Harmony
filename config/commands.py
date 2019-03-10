8634560a932ac12fd16fa956de0832758c8ab37a
Tim Nicholls <cx004763@live.reading.ac.uk>
2019-03-10 03:43:01 +0000

    Harmonized

--- config/commands.py
+++ config/commands.py
@@ -1,18 +1,6 @@
-from MessageHandler.Command.Commands.Escape import Escape
 from MessageHandler.Command.Commands.Hello import Hello
-from MessageHandler.Command.Commands.Pokecord import Pokecord
-from MessageHandler.Command.Commands.Translation.Eng2Rus import Eng2Rus
-from MessageHandler.Command.Commands.Translation.Rus2Eng import Rus2Eng
-from MessageHandler.Command.Commands.Whois import Whois
 
 commands = [
     # Default
-    Hello(),
-    # Generic
-    Escape(),
-    Pokecord(),
-    Whois(),
-    # Translation
-    Eng2Rus(),
-    Rus2Eng()
+    Hello()
 ]
